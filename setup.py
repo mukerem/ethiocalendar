8ff90188c72953cdb6b896e792df1946bcedc016
Apple <apple@appurunoMacBook-Air.local>
2023-04-04 14:47:45 +0300

    build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'ethiocalendar',         # How you named your package folder (MyLib)
   packages = ['ethiocalendar'],   # Chose the same as "name"
-  version = '1.2',      # Start with a small number and increase it with every change you make
+  version = '1.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Ethiopian Calendar based date and time module',   # Give a short description about your library
   author = 'Mukerem Ali',                   # Type in your name
