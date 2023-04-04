a3505810f9e04692782521a4faa9a9b411714ee3
Apple <apple@appurunoMacBook-Air.local>
2023-04-04 15:01:59 +0300

    fix: version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ethiocalendar',         # How you named your package folder (MyLib)
   packages = ['ethiocalendar'],   # Chose the same as "name"
-  version = '1.0.2',      # Start with a small number and increase it with every change you make
+  version = '1.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Ethiopian Calendar based date and time module',   # Give a short description about your library
   author = 'Mukerem Ali',                   # Type in your name
   author_email = 'mukeremali112@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/mukerem/ethiocalendar',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mukerem/ethiocalendar/archive/refs/tags/v1.0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mukerem/ethiocalendar/archive/refs/tags/v1.1.2.tar.gz',    # I explain this later on
   install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
